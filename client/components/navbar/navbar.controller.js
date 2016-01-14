b49045c9313bd438b60fc7438dc3073f776ffefe
Rumman Ahmed <ahmed.rumman@gmail.com>
2016-01-14 17:16:34 +1100

    more modification made

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -5,32 +5,13 @@ angular.module('finapp')
     $scope.menu               = [
       // {'title': 'Home','link': '/'},
       {'title': 'Accounting','link':'/accounting','popMsg':'Accounting','acl':'user'},
-      {'title': 'Display','link':'/display','popMsg':'Display','acl':'user'},
       {'title': 'Chart','link':'/chart','popMsg':'Chart','acl':'user'},
       {'title': 'CSV','link':'/csv','popMsg':'CSV','acl':'user'},
-      {'title': 'D3','link':'/d3','popMsg':'D3','acl':'user'},
-      {'title': 'Poll','link':'/poll','popMsg':'Poll','acl':'user'},
-      {'title': 'ElasticSearch','link':'/elastic','popMsg':'Elastic Search','acl':'user'},
-      {'title': 'Dashboard','link':'/dashboard','popMsg':'Dashboard','acl':'user'},
-      {'title': 'Chat','link':'/chat','popMsg':'Chat','acl':'user'},
-      {'title': 'VideoChat','link':'/videochat','popMsg':'Video Chat','acl':'user'},
-      {'title': 'Phones','link':'/phones','popMsg':'Phones','acl':'user'},
-      {'title': 'Speak','link':'/speak','popMsg':'Speak','acl':'user'},
-      {'title': 'Scroll','link':'/scroll','popMsg':'Scroll','acl':'user'},
-      {'title': 'Forms','link':'/forms','popMsg':'Forms','acl':'user'},
-      {'title': 'Cart','link':'/cart','popMsg':'Cart','acl':'user'},
-      {'title': 'Checkout','link':'/checkout','popMsg':'Checkout','acl':'user'}
+      {'title': 'D3','link':'/d3','popMsg':'D3','acl':'user'}
       ];
     $scope.loggedinMenu       = [
-      {'title': 'Checkout','link':'/checkout','popMsg':'Application Repository','acl':'loggeduser'}
      ]; 
     $scope.SuperloggedinMenu  = [
-      {'title': 'Tabs','link':'/tabs','popMsg':'Tabs','acl':'user'},
-      {'title': 'Template','link':'/template','popMsg':'Template','acl':'user'},
-      {'title': 'Monad','link':'/monad','popMsg':'Monad','acl':'user'},
-      {'title': 'Polymer','link':'/polymer','popMsg':'Polymer','acl':'user'},
-      {'title': 'React','link':'/react','popMsg':'React','acl':'user'},
-      // {'title': 'Upload','link':'/upload','popMsg':'Upload','acl':'user'}
      ]; 
 
     $scope.isCollapsed    = true;
